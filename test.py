70091265d1b71d1fe88a40ed8a6eb098dc43c3b3
Dongze He <32473855+DongzeHE@users.noreply.github.com>
2020-12-13 17:12:23 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -30,72 +30,86 @@ import scipy.sparse as sparse
 
 print("Loading data...")
 
-t0=time()
-file_path = "../DATA/GSE131928_RAW/GSM3828672_Smartseq2_GBM_IDHwt_processed_TPM.tsv"
+# t0=time()
+file_path = "DATA/GSM3828672_Smartseq2_GBM_IDHwt_processed_TPM.tsv"
 df = pd.read_csv(file_path, sep='\t').transpose()
-print("done in %0.3fs." % (time() - t0))
+# print("done in %0.3fs." % (time() - t0))
 
-print("Filtering genes...")
-t0=time()
+# print("Filtering genes...")
+# t0=time()
 
-# extract gene names and expression values
+# extract gene names
 gene_names = df.values[0]
-exp = np.round(normalize(df.values[1:],axis=1,norm='max')*100)
+
+# normalize expression value by cell, so all cells are in the same level.
+exp_cell_normalized = np.round(normalize(df.values[1:],axis=1,norm='max')*100)
 
 # Keep the top 2000 genes with the highest variance
-var = exp.var(axis = 0)
-ind = np.argpartition(var, -1500)[-1500:]
+var = exp_cell_normalized.var(axis = 0)
 
-print("done in %0.3fs." % (time() - t0))
+num_genes_list = [500,1000,1500]
 
-print("Constructing sparse matrix...")
-t0=time()
+num_topics = [3,5,7,9,11]
 
-tf = sparse.csr_matrix(exp[:,ind].astype(float))
-tf_feature_names = gene_names[ind]
-del(df,exp,gene_names,var,ind)
+for n_genes in num_genes_list:
+    ind = np.argpartition(var, -n_genes)[-n_genes:]
 
-print("done in %0.3fs." % (time() - t0))
+    # print("done in %0.3fs." % (time() - t0))
 
-n_samples = tf.shape[0]
-n_features = tf.shape[1]
-n_components = 4
-n_top_words = 30
+    # print("Constructing sparse matrix...")
+    # t0=time()
 
+    tf = sparse.csr_matrix(exp_cell_normalized[:,ind].astype(float))
+    tf_feature_names = gene_names[ind]
 
+    with open("results/filtered_gene%d_exp.pkl"%(n_genes), 'wb') as f:  # Python 3: open(..., 'wb')
+        pickle.dump([tf,tf_feature_names], f,-1)
 
+    # print("done in %0.3fs." % (time() - t0))
 
-def print_top_words(model, feature_names, n_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        topic_prior = model.alpha_[topic_idx]
-        message = "Topic #%d (prior: %.3f): " % (topic_idx, topic_prior)
-        message += " ".join([feature_names[i]
-                             for i in topic.argsort()[:-n_top_words - 1:-1]])
-        print(message)
-    print()
+    n_samples = tf.shape[0]
+    n_features = tf.shape[1]
+    n_top_words = 30
 
+    for n_components in num_topics:
 
-# Load the 20 newsgroups dataset and vectorize it. We use a few heuristics
-# to filter out useless terms early on: the posts are stripped of headers,
-# footers and quoted replies, and common English words, words occurring in
-# only one document or in at least 95% of the documents are removed.
-
-
-print("Doing tensor LDA...")
-t0=time()
-
-lda = TensorLDA(n_components=n_components, alpha0=.01)
-
-t0 = time()
-lda.fit(tf)
-print("done in %0.3fs." % (time() - t0))
-
-print("\nTopics in LDA model:")
-print_top_words(lda, tf_feature_names, n_top_words)
+        def print_top_words(model, feature_names, n_top_words):
+            for topic_idx, topic in enumerate(model.components_):
+                topic_prior = model.alpha_[topic_idx]
+                message = "Topic #%d (prior: %.3f): " % (topic_idx, topic_prior)
+                message += " ".join([feature_names[i]
+                                     for i in topic.argsort()[:-n_top_words - 1:-1]])
+                print(message)
+            print()
 
 
-with open('lda_model.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
-    pickle.dump(lda, f,-1)
+        # Load the 20 newsgroups dataset and vectorize it. We use a few heuristics
+        # to filter out useless terms early on: the posts are stripped of headers,
+        # footers and quoted replies, and common English words, words occurring in
+        # only one document or in at least 95% of the documents are removed.
+
+
+        # print("Doing tensor LDA...")
+        # t0=time()
+
+        lda = TensorLDA(n_components=n_components, alpha0=.01)
+
+        # t0 = time()
+        lda.fit(tf)
+        # print("done in %0.3fs." % (time() - t0))
+
+        prediction = lda.transform(tf).argmax(axis=1)
+        print("\n")
+
+        print("# of topic: %d\t # of gene: %d" % (n_components, n_genes))
+
+        print("Topics in LDA model:")
+        print_top_words(lda, tf_feature_names, n_top_words)
+        print("\n\n")
+
+
+        with open("results/lda_model_topic%d_gene%d.pkl"%(n_components, n_genes), 'wb') as f:  # Python 3: open(..., 'wb')
+            pickle.dump(lda, f,-1)
 
 # Getting back the objects:
 # with open('objs.pkl') as f:  # Python 3: open(..., 'rb')
